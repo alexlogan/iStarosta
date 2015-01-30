1d5c51bce086199c37081671d5470a31e5356a87
Alex <g6morg@gmail.com>
2015-01-30 15:25:01 +0300

    init commit

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,8 @@
 class Lesson < ActiveRecord::Base
   has_many :logs, dependent: :delete_all
   has_one :absence, dependent: :delete
+
+  after_save :create_absence
   validates :name,
             presence:true,
             uniqueness: true,
@@ -8,4 +10,11 @@ class Lesson < ActiveRecord::Base
               with: /\A[а-яА-Я0-9.\s]+/,
               message: "only allows letters, numbers and \".\""
             }
+
+  private
+  def create_absence
+    Student.all.each do |student|
+      Absence.create({student_id: student.id, lesson_id: self.id, amount: 0})
+    end
+  end
 end
