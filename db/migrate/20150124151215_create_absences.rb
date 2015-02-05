5df6af4e9cc794b0a4220ffed1d91ceb19e8005f
Alex <g6morg@gmail.com>
2015-02-05 14:31:51 +0300

    some localization and few fix

--- db/migrate/20150124151215_create_absences.rb
+++ db/migrate/20150124151215_create_absences.rb
@@ -3,7 +3,7 @@ class CreateAbsences < ActiveRecord::Migration
     create_table :absences do |t|
       t.references :student, null: false
       t.references :lesson, null: false
-      t.integer :amount, null: false
+      t.integer :amount, null: false, default: 0
       t.timestamps null: false
     end
   end
