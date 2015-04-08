4cd2a7e8c52e614d839b23796cef068709880ab8
Alex <g6morg@gmail.com>
2015-04-08 19:36:28 +0300

    add default to transaction_id column

--- db/migrate/20150408143811_add_transaction_id_to_logs.rb
+++ db/migrate/20150408143811_add_transaction_id_to_logs.rb
@@ -1,5 +1,5 @@
 class AddTransactionIdToLogs < ActiveRecord::Migration
   def change
-    add_column :logs, :transaction_id, :integer, null: false
+    add_column :logs, :transaction_id, :integer, null: false, default: 0
   end
 end
